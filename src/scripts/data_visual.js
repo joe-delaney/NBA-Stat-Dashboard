31d98a792306f212cae32850cc86e01c3b4c33df
joe-delaney <joseph.delaney546@gmail.com>
2022-04-04 14:43:22 -0400

    refactored data visual class to be more DRY

--- src/scripts/data_visual.js
+++ src/scripts/data_visual.js
@@ -6,10 +6,14 @@ export default class DataVisual {
             this.height = 600 - this.margin.top - this.margin.bottom;
     }
 
+    //Used as a controller to direct to the correct draw chart method
     drawChart(tag, seasons, chartData) {
+        this.metricLabel = chartData[0].metricLabel;
         switch (tag) {
             case "line":
                 this.drawLineChart(seasons, chartData);
+            case "bar":
+                this.drawBarChart(seasons, chartData);
             default:
                 break;
         } 
@@ -17,15 +21,7 @@ export default class DataVisual {
     }
 
     drawLineChart(seasons, chartData) {
-        let metricLabel = chartData[0].metricLabel;
-
-        this.svg = d3.select("#data-visualization")
-            .append("svg")
-            .attr("width", this.width + this.margin.left + this.margin.right)
-            .attr("height", this.height + this.margin.top + this.margin.bottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + this.margin.left + "," + this.margin.top + ")");
+        let svg = this.getSVG();
 
         //Group the data by player
         let players = d3.nest()
@@ -38,7 +34,7 @@ export default class DataVisual {
             .range([0, 1000]);
 
         //Add the X axis
-        this.svg.append("g")
+        svg.append("g")
             .attr("class", "axis")
             .attr("transform", "translate(0," + this.height + ")")
             .call(d3.axisBottom()
@@ -46,7 +42,7 @@ export default class DataVisual {
                 .tickFormat(d3.format('d'))
                 .tickValues(d3.range(Math.min.apply(Math, seasons), Math.max.apply(Math, seasons) + 1, 1)));
         
-        this.svg.append("text")
+        svg.append("text")
             .attr("class", "axis-label")
             .attr("text-anchor", "end")
             .attr("x", 520)
@@ -59,28 +55,22 @@ export default class DataVisual {
             .range([this.height, 30]);
 
         // Add Y axis
-        this.svg.append("g")
+        svg.append("g")
             .call(d3.axisLeft().scale(y));
         
-        this.svg.append("text")
+        svg.append("text")
             .attr("class", "axis-label")
             .attr("text-anchor", "end")
             .attr("y", -50)
             .attr("x", -this.height / 3)
             .attr("dy", ".75em")
             .attr("transform", "rotate(-90)")
-            .text(this.getYAxisLabel(metricLabel));
+            .text(this.getYAxisLabel(this.metricLabel));
 
-        console.log(players);
-
-        let res = players.map(function (d) { return d.key }) // list of players
-        let color = d3.scaleOrdinal()
-            .domain(res)
-            .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'])
-
+        let color = this.getColorScheme(players);
 
         // Add the lines
-        this.svg.selectAll(".line")
+        svg.selectAll(".line")
             .data(players)
             .enter()
             .append("path")
@@ -97,40 +87,22 @@ export default class DataVisual {
             })    
             
         //add legend
-        var legend = d3.select("svg")
-            .selectAll('g.legend')
-            .data(players)
-            .enter()
-            .append("g")
-            .attr("class", "legend");
-
-        legend.append("circle")
-            .attr("cx", 1100)
-            .attr('cy', (d, i) => i * 30 + 350)
-            .attr("r", 6)
-            .style("fill", d => color(d.key))
-
-        legend.append("text")
-            .attr("x", 1120)
-            .attr("y", (d, i) => i * 30 + 355)
-            .text(d => d.key)
+        this.addLegend(players, color);
         
         //add title
-        d3.select("svg")
-            .append("text")
-            .attr("x", this.margin.left)
-            .attr("y", 20)
-            .attr("text-anchor", "left")
-            .text(`${this.getYAxisLabel(metricLabel)} from ${seasons[0]} to ${seasons[seasons.length-1]}`)
-            .style("fill", "black")
-            .style("font-size", 16)
-            .style("font-family", "Arial Black")
+        this.addTitle(seasons);
+    }
+
+    drawBarChart(seasons, chartData) {
+
     }
 
     //Clear the current chart
     reset() {
         d3.selectAll("svg").remove();
     }
+
+    //The following methods will be used by each chart
 
     getYAxisLabel(metricLabel) {
         switch (metricLabel) {
@@ -150,4 +122,59 @@ export default class DataVisual {
                 break;
         }
     }
+
+    //return the svg to render the chart on
+    getSVG() {
+        return d3.select("#data-visualization")
+            .append("svg")
+            .attr("width", this.width + this.margin.left + this.margin.right)
+            .attr("height", this.height + this.margin.top + this.margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + this.margin.left + "," + this.margin.top + ")");
+    }
+
+    //map colors to players return the color scheme to be used in the current chart
+    getColorScheme(players) {
+        let res = players.map(function (d) { return d.key }) // list of players
+        let color = d3.scaleOrdinal()
+            .domain(res)
+            .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'])
+        return color;
+    }
+
+    //add legend to the current chart
+    addLegend(players, color) {
+        var legend = d3.select("svg")
+            .selectAll('g.legend')
+            .data(players)
+            .enter()
+            .append("g")
+            .attr("class", "legend");
+
+        legend.append("circle")
+            .attr("cx", 1100)
+            .attr('cy', (d, i) => i * 30 + 350)
+            .attr("r", 6)
+            .style("fill", d => color(d.key))
+
+        legend.append("text")
+            .attr("x", 1120)
+            .attr("y", (d, i) => i * 30 + 355)
+            .text(d => d.key);
+    }
+
+    //add title to the current chart
+    addTitle(seasons) {
+        d3.select("svg")
+            .append("text")
+            .attr("x", this.margin.left)
+            .attr("y", 20)
+            .attr("text-anchor", "left")
+            .text(`${this.getYAxisLabel(this.metricLabel)} from ${seasons[0]} to ${seasons[seasons.length - 1]}`)
+            .style("fill", "black")
+            .style("font-size", 16)
+            .style("font-family", "Arial Black")
+    }
+
 }
